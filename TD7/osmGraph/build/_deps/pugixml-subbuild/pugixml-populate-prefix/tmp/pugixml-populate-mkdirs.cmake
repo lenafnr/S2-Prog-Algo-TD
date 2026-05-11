416156af1a85b0dda339c4dfc42d8d19bed27f01
lenafnr <166299651+lenafnr@users.noreply.github.com>
2026-05-11 21:07:13 +0200

    added exo 7

---
+++ TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/tmp/pugixml-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-src")
+  file(MAKE_DIRECTORY "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-src")
+endif()
+file(MAKE_DIRECTORY
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-build"
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix"
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/tmp"
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/src/pugixml-populate-stamp"
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/src"
+  "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/src/pugixml-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/src/pugixml-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/pugixml-subbuild/pugixml-populate-prefix/src/pugixml-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ TD7/osmGraph/build/_deps/raylib-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/raylib-src
+# Build directory: /Users/lenafournier/Desktop/IMAC/IMAC1/ProgAlgo/S2-Prog-Algo-TD/TD7/osmGraph/build/_deps/raylib-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
